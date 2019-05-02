e45f8a9573907718cec16da7d672a63009ecac10
Gry Nagel <grynag@gmail.com>
2019-05-02 09:24:08 +0200

    Adding examples

---
+++ 1-Getting-started/SettingUpTheProject/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+
+    public float fallHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + fallHeight, transform.position.z); 
+    }
+
+}
---
+++ 2-Unity-Introduction/UnityIntroduction/Assets/Scripts/Trigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject otherObj = collision.gameObject;
+        Debug.Log("Collided with: " + otherObj);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject otherObj = collider.gameObject;
+        Debug.Log("Triggered with: " + otherObj);
+    }
+}

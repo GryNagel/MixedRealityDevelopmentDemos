6f7127a5530d767bea779b61a1fbd3bdc4e09d7c
Gry Nagel <grynag@gmail.com>
2019-05-05 10:03:39 +0200

    Demos

---
+++ 3-MRTK-Features/Demo/Assets/Scripts/Buttons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public float moveDistance; 
+
+    private Vector3 cubePosition; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubePosition = transform.position; 
+    }
+
+    public void MoveCube(string direction)
+    {
+     
+        if (direction == "left")
+        {
+            transform.position = new Vector3(transform.position.x - moveDistance, transform.position.y, transform.position.z);
+        }
+        else if(direction == "right")
+        {
+            transform.position = new Vector3(transform.position.x + moveDistance, transform.position.y, transform.position.z);
+        }
+    }
+
+    public void ResetCubePosition()
+    {
+        transform.position = cubePosition;
+    }
+}

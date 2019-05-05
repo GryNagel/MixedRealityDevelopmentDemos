6bb469c597e2cb0c752370cabb637ba350720c59
Gry Nagel <grynag@gmail.com>
2019-05-05 22:57:55 +0200

    Solver and builds

---
+++ 3-MRTK-Features/Demo/Assets/Scripts/PrefabLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabLoader : MonoBehaviour
+{
+    
+    public void SpawnPrefab(GameObject prefab)
+    {
+        Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
